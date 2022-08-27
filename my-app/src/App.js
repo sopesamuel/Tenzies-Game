2aae34300208eaf7a59befab6476944e2972b46c
sopesamuel <fasusimosope@gmail.com>
2022-08-28 00:53:14 +0100

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,40 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Main from './Main';
+import Die from "./Die";
 import './App.css';
 
 export default function App() {
+
+  function randomNumber(){
+    const newArray = [];
+for(let i = 0; i < 10; i++){
+  newArray.push( {
+    value: Math.floor(Math.random() * 6),
+    isHeld: false
+    })
+  
+}
+    return newArray
+  }
+  
+  const [dice, setDice] = React.useState(randomNumber());
+  const diceElements = dice.map(die => <Die value={die.value}/>)
+  
+  function newNumber(){
+   setDice(randomNumber())
+  }
+
   return (
-    <div className="App">
-      <Main />
+    <main>
+    <div className="die-component" >
+    {diceElements}
     </div>
+    <button 
+    className="btn"
+    onClick={newNumber}
+    >Roll</button>
+    </main> 
+    
   );
 }
 
---
+++ my-app/src/Die.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Die(props){
+    return (
+        <div className="die-list">
+<h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
