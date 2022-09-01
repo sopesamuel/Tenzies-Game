f2347278b86c5057a43d071cba1d3c45e4e8646a
sopesamuel <fasusimosope@gmail.com>
2022-09-01 10:48:32 +0100

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,49 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Die from "./Die";
+import Die from "./Die.js";
 import './App.css';
+import {nanoid} from "nanoid"
 
 export default function App() {
 
+  const [dice, setDice] = React.useState(randomNumber());
+
+  function getAnotherNumber(){
+   return  {
+      value: Math.floor(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+      }
+  }
+
   function randomNumber(){
     const newArray = [];
-for(let i = 0; i < 10; i++){
-  newArray.push( {
-    value: Math.floor(Math.random() * 6),
-    isHeld: false
-    })
-  
+    for(let i = 0; i < 10; i++){
+    newArray.push(getAnotherNumber())
 }
     return newArray
   }
   
-  const [dice, setDice] = React.useState(randomNumber());
-  const diceElements = dice.map(die => <Die value={die.value}/>)
+  function holdDice(id){
+  setDice(oldDice => oldDice.map(die => {
+return die.id === id ? {...die, isHeld: !die.isHeld} : die
+  } ))
+  // console.log(id)
+  }
+
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>)
   
   function newNumber(){
-   setDice(randomNumber())
+   setDice(oldDice => oldDice.map(die =>{
+   return  die.isHeld ? die : getAnotherNumber()
+   }))
   }
 
   return (
     <main>
+    <h1 className="title">Tenzies</h1>
+    <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
     <div className="die-component" >
     {diceElements}
     </div>
@@ -36,6 +54,6 @@ for(let i = 0; i < 10; i++){
     </main> 
     
   );
-}
+} 
 
 ReactDOM.render(<App />, document.getElementById("root"))
--- my-app/src/Die.js
+++ my-app/src/Die.js
@@ -1,9 +1,10 @@
 import React from "react"
 
 export default function Die(props){
+    const styles = { backgroundColor: props.isHeld ? "#59E391" : "white"}
     return (
-        <div className="die-list">
-<h2 className="die-num">{props.value}</h2>
+        <div className="die-list" style={styles} onClick={props.holdDice}>
+        <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
